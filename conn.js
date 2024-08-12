c0164c14115b5a71da7a40e577d4474d3aeab77e
Hemmaps <hemmapssivagnanam@gmail.com>
2024-08-12 11:46:59 +0530

    deploy

--- conn.js
+++ conn.js
@@ -1,4 +1,8 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/foodData')
+mongoose.connect(process.env.MONGODB_URI)
 .then(()=>{console.log('Database is connected')})
 .catch(()=>{console.log('Database is not connected')})
